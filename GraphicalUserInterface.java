f7c4cb0a328b3d8a00db95d2dbba39bb98df5bde
giangelo19 <tongzongian@gmail.com>
2025-05-03 16:44:50 +0800

    Update GraphicalUserInterface.java

--- GraphicalUserInterface.java
+++ GraphicalUserInterface.java
@@ -1,13 +1,23 @@
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.SwingConstants;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Cursor;
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.SwingConstants;
 
 public class GraphicalUserInterface implements ActionListener {
 
