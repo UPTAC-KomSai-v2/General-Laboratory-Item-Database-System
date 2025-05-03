6449311ea11a41f13cd79d3468e0baa5ee6f48a9
makkudaren <macalimba@up.edu.ph>
2025-05-03 20:01:35 +0800

    Draft Borrow Items Panel

--- GUIBorrowItemPanel.java
+++ GUIBorrowItemPanel.java
@@ -1,4 +1,5 @@
 import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
@@ -19,13 +20,15 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
-public class GUIBorrowItemPanel extends JPanel {
+public class GUIBorrowItemPanel extends JPanel{
+    private CardLayout cardLayout;
     private Branding branding;
-    private JPanel mainContentPanel;
+    private JPanel screen1, screen2, screen3;
     private JButton selectedCategoryButton = null;
+    private JPanel scrn2BorrowedItemsContentPanel;
     private JPanel equipmentPanel;  // Declare equipmentPanel as an instance variable
     private JPanel selectedItemCard = null; // Track the currently selected equipment item
-    private JButton addToBasketButton; // Track the Add To Basket button
+    private JButton addToBasketButton, continueButton; // Track the Add To Basket button
     
     // Data structure to keep track of items in the basket
     private List<BasketItem> basketItems = new ArrayList<>();
@@ -50,14 +53,19 @@ public class GUIBorrowItemPanel extends JPanel {
     
     public GUIBorrowItemPanel(Branding branding, JButton backButton) {
         this.branding = branding;
-        this.setLayout(new BorderLayout());
+        this.cardLayout = new CardLayout();
+        this.setLayout(cardLayout);
         this.setBackground(Color.WHITE);
         this.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         this.setPreferredSize(new Dimension(900, 500));
         
         // Create main content panel
-        initializeMainContentPanel(backButton);
-        add(mainContentPanel, BorderLayout.CENTER);
+        initializeScreen1(backButton);
+        initializeScreen2();
+        initializeScreen3();
+        add(screen1, "Panel 1");
+        add(screen2, "Panel 2");
+        add(screen3, "Panel 3");
         
     }
 
@@ -93,10 +101,10 @@ public class GUIBorrowItemPanel extends JPanel {
          "Safety Boots", "Gas Mask", "Lab Apron", "Safety Signs", "Ear Protection", "Fire Hose", "Spill Kit", "Fume Hood"}
     };
     
-    private void initializeMainContentPanel(JButton backButton) {
+    private void initializeScreen1(JButton backButton) {
         // Main panel with border layout
-        mainContentPanel = new JPanel(new BorderLayout());
-        mainContentPanel.setBorder(BorderFactory.createLineBorder(branding.maroon, 2));
+        screen1 = new JPanel(new BorderLayout());
+        screen1.setBorder(BorderFactory.createLineBorder(branding.maroon, 2));
         
         // Create content panel with border layout
         JPanel contentPanel = new JPanel(new BorderLayout());
@@ -110,12 +118,235 @@ public class GUIBorrowItemPanel extends JPanel {
         equipmentPanel = createEquipmentPanel(backButton);
         contentPanel.add(equipmentPanel, BorderLayout.CENTER);
         
-        mainContentPanel.add(contentPanel, BorderLayout.CENTER);
+        screen1.add(contentPanel, BorderLayout.CENTER);
         
         // Show the initial "No Category is Selected" message
         showNoCategorySelectedMessage();
     }
     
+    private void initializeScreen2(){
+        screen2 = new JPanel();
+        screen2.setLayout(new GridBagLayout());
+        screen2.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0));
+        screen2.setBackground(branding.maroon);
+
+        scrn2BorrowedItemsContentPanel = new JPanel();
+        scrn2BorrowedItemsContentPanel.setLayout(new BoxLayout(scrn2BorrowedItemsContentPanel, BoxLayout.Y_AXIS));
+        scrn2BorrowedItemsContentPanel.setBackground(branding.maroon);
+        scrn2BorrowedItemsContentPanel.setBorder(BorderFactory.createEmptyBorder(0, 30, 30, 30));
+        
+        
+
+        String[] borrowerInfo = {"Example, John Pork", "69696969", "12:01 PM", "13 Feb 2023"};
+
+        JPanel scrn2BorrowerInfoPanel = new JPanel();
+        scrn2BorrowerInfoPanel.setBorder(BorderFactory.createEmptyBorder(10, 50, 20, 0));
+        scrn2BorrowerInfoPanel.setLayout(new GridBagLayout());
+        scrn2BorrowerInfoPanel.setOpaque(false);
+        int i = 0;
+        JLabel borrowerLabel = new JLabel("Borrower: ");
+        JLabel borrowerNameLabel = new JLabel(borrowerInfo[i++]);
+        JLabel studentIdLabel = new JLabel(borrowerInfo[i++]);
+        JLabel timeLabel = new JLabel(borrowerInfo[i++]);
+        JLabel dateLabel = new JLabel(borrowerInfo[i++]);
+
+        borrowerLabel.setForeground(branding.white);
+        borrowerNameLabel.setForeground(branding.white);
+        studentIdLabel.setForeground(branding.white);
+        timeLabel.setForeground(branding.white);
+        dateLabel.setForeground(branding.white);
+
+        JPanel borrowerPanel = new JPanel();
+        JPanel borrowerNamePanel = new JPanel();
+        JPanel studentIdPanel = new JPanel();
+        JPanel timePanel = new JPanel();
+        JPanel datePanel = new JPanel();
+
+        borrowerPanel.setLayout(new BorderLayout());
+        borrowerNamePanel.setLayout(new BorderLayout());
+        studentIdPanel.setLayout(new BorderLayout());
+        timePanel.setLayout(new BorderLayout());
+        datePanel.setLayout(new BorderLayout());
+
+        borrowerPanel.setOpaque(false);
+        borrowerNamePanel.setOpaque(false);
+        studentIdPanel.setOpaque(false);
+        timePanel.setOpaque(false);
+        datePanel.setOpaque(false);
+
+        borrowerPanel.add(borrowerLabel, BorderLayout.WEST);
+        borrowerNamePanel.add(borrowerNameLabel, BorderLayout.WEST);
+        studentIdPanel.add(studentIdLabel, BorderLayout.WEST);
+        timePanel.add(timeLabel, BorderLayout.WEST);
+        datePanel.add(dateLabel, BorderLayout.WEST);
+
+        GridBagConstraints scrn2BorrowerInfoPanelGBC = new GridBagConstraints();
+        scrn2BorrowerInfoPanelGBC.fill = GridBagConstraints.HORIZONTAL;
+        scrn2BorrowerInfoPanelGBC.gridx = 0;
+        scrn2BorrowerInfoPanelGBC.ipadx = 20;
+        scrn2BorrowerInfoPanelGBC.weightx = 0.05;
+        scrn2BorrowerInfoPanel.add(borrowerPanel, scrn2BorrowerInfoPanelGBC);
+        scrn2BorrowerInfoPanelGBC.gridx++;
+        scrn2BorrowerInfoPanelGBC.ipadx = 20;
+        scrn2BorrowerInfoPanelGBC.weightx = 0.1;
+        scrn2BorrowerInfoPanel.add(borrowerNamePanel, scrn2BorrowerInfoPanelGBC);
+        scrn2BorrowerInfoPanelGBC.gridx++;
+        scrn2BorrowerInfoPanelGBC.ipadx = 20;
+        scrn2BorrowerInfoPanelGBC.weightx = 0.05;
+        scrn2BorrowerInfoPanel.add(studentIdPanel, scrn2BorrowerInfoPanelGBC);
+        scrn2BorrowerInfoPanelGBC.gridx++;
+        scrn2BorrowerInfoPanelGBC.ipadx = 20;
+        scrn2BorrowerInfoPanelGBC.weightx = 0.05;
+        scrn2BorrowerInfoPanel.add(timePanel, scrn2BorrowerInfoPanelGBC);
+        scrn2BorrowerInfoPanelGBC.gridx++;
+        scrn2BorrowerInfoPanelGBC.ipadx = 20;
+        scrn2BorrowerInfoPanelGBC.weightx = 0.5;
+        scrn2BorrowerInfoPanel.add(datePanel, scrn2BorrowerInfoPanelGBC);
+
+        JScrollPane scrn2ScrollContentPanel = new JScrollPane(scrn2BorrowedItemsContentPanel);
+        scrn2ScrollContentPanel.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrn2ScrollContentPanel.getVerticalScrollBar().setUnitIncrement(16);
+        scrn2ScrollContentPanel.setBackground(branding.maroon);
+        scrn2ScrollContentPanel.setBorder(null);
+        branding.reskinScrollBar(scrn2ScrollContentPanel, branding.maroon);
+
+        JPanel scrn2MenuPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT, 15, 10));
+        scrn2MenuPanel.setBorder(BorderFactory.createEmptyBorder(10, 5, 5, 33));
+        scrn2MenuPanel.setBackground(branding.lightgray);
+        scrn2MenuPanel.setOpaque(true);
+
+        JButton screen2BackBtn = new JButton("Go Back");
+        JButton screen2ContinueBtn = new JButton("Continue");
+
+
+        screen2BackBtn.setPreferredSize(new Dimension(150, 30));
+        screen2BackBtn.setBackground(branding.maroon);
+        screen2BackBtn.setForeground(branding.white);
+        screen2BackBtn.addActionListener(e -> {
+            cardLayout.previous(GUIBorrowItemPanel.this);
+        });
+
+        screen2ContinueBtn.setPreferredSize(new Dimension(150, 30));
+        screen2ContinueBtn.setBackground(branding.maroon);
+        screen2ContinueBtn.setForeground(branding.white);
+        screen2ContinueBtn.addActionListener(e -> {
+            cardLayout.next(GUIBorrowItemPanel.this);
+        });
+
+
+
+        scrn2MenuPanel.add(screen2BackBtn);
+        scrn2MenuPanel.add(screen2ContinueBtn);
+
+        GridBagConstraints screen2GBC = new GridBagConstraints();
+        screen2GBC.fill = GridBagConstraints.BOTH;
+        screen2GBC.weightx = 1;
+        screen2GBC.weighty = 0.01;
+        screen2GBC.gridy = 0;
+        screen2.add(scrn2BorrowerInfoPanel, screen2GBC);
+        screen2GBC.weighty = 0.9;
+        screen2GBC.gridy++;
+        screen2.add(scrn2ScrollContentPanel, screen2GBC);
+        screen2GBC.weighty = 0.01;
+        screen2GBC.gridy++;
+        screen2.add(scrn2MenuPanel, screen2GBC);
+
+    }
+
+    private void initializeScreen3(){
+        screen3 = new JPanel();
+        screen3.setLayout(new GridBagLayout());
+        screen3.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0));
+        screen3.setBackground(branding.lightgray);
+
+        JButton screen3BackBtn = new JButton("Go Back");
+        screen3BackBtn.addActionListener(e ->{
+            cardLayout.previous(GUIBorrowItemPanel.this);
+        });
+
+        screen3.add(screen3BackBtn);
+    }
+
+    private void updateBasketDisplayPanel(){
+        String[][] entries = {
+            {"2x", "Beaker", "100 ml"},
+            {"1x", "Petri Dish", ""}
+        };
+        
+        for (String[] tuple: entries){
+            int i = 0;
+            
+            JLabel quantityLabel = new JLabel(tuple[i++]);
+            JLabel itemLabel = new JLabel(tuple[i++]);
+            JLabel unitLabel = new JLabel(tuple[i++]);
+
+            quantityLabel.setForeground(branding.maroon);
+            itemLabel.setForeground(branding.maroon);
+            unitLabel.setForeground(branding.maroon);
+
+            JButton returnItemBtn = new JButton("Return");
+            returnItemBtn.setPreferredSize(new Dimension(100, 35));
+            returnItemBtn.setBackground(branding.maroon);
+            returnItemBtn.setForeground(branding.white);
+            
+            
+            JPanel quantityPanel = new JPanel();
+            JPanel itemPanel = new JPanel();
+            JPanel unitPanel = new JPanel();
+            JPanel returnPanel = new JPanel();
+
+            quantityPanel.setLayout(new BorderLayout());
+            itemPanel.setLayout(new BorderLayout());
+            unitPanel.setLayout(new BorderLayout());
+            returnPanel.setLayout(new GridBagLayout());
+
+            quantityPanel.setPreferredSize(new Dimension(10, 70));
+            itemPanel.setPreferredSize(new Dimension(10, 70));
+            unitPanel.setPreferredSize(new Dimension(10, 70));
+            returnPanel.setPreferredSize(new Dimension(10, 70));
+            
+            quantityPanel.setOpaque(false);
+            itemPanel.setOpaque(false);
+            unitPanel.setOpaque(false);
+            returnPanel.setOpaque(false);
+
+            quantityPanel.add(quantityLabel, BorderLayout.WEST);
+            itemPanel.add(itemLabel, BorderLayout.WEST);
+            unitPanel.add(unitLabel, BorderLayout.WEST);
+            returnPanel.add(returnItemBtn);
+
+            JPanel tupleInfoPanel = new JPanel();
+            tupleInfoPanel.setMaximumSize(new Dimension(900, 70));
+            tupleInfoPanel.setBorder(BorderFactory.createEmptyBorder(0, 50, 0, 20));
+            tupleInfoPanel.setLayout(new GridBagLayout());
+            tupleInfoPanel.setBackground(branding.lightgray);
+            
+            GridBagConstraints tupleInfoPanelGBC = new GridBagConstraints();
+            tupleInfoPanelGBC.fill = GridBagConstraints.HORIZONTAL;
+            tupleInfoPanelGBC.gridx = 0;
+            tupleInfoPanelGBC.ipadx = 20;
+            tupleInfoPanelGBC.weightx = 0.05;
+            tupleInfoPanel.add(quantityPanel, tupleInfoPanelGBC);
+            tupleInfoPanelGBC.gridx++;
+            tupleInfoPanelGBC.ipadx = 20;
+            tupleInfoPanelGBC.weightx = 0.1;
+            tupleInfoPanel.add(itemPanel, tupleInfoPanelGBC);
+            tupleInfoPanelGBC.gridx++;
+            tupleInfoPanelGBC.ipadx = 20;
+            tupleInfoPanelGBC.weightx = 0.1;
+            tupleInfoPanel.add(unitPanel, tupleInfoPanelGBC);
+            tupleInfoPanelGBC.fill = GridBagConstraints.NONE;
+            tupleInfoPanelGBC.anchor = GridBagConstraints.EAST; // optional
+            tupleInfoPanelGBC.gridx++;
+            tupleInfoPanelGBC.ipadx = 100;
+            tupleInfoPanelGBC.weightx = 0.2;
+            tupleInfoPanel.add(returnPanel, tupleInfoPanelGBC);
+
+            scrn2BorrowedItemsContentPanel.add(tupleInfoPanel);
+            scrn2BorrowedItemsContentPanel.add(Box.createVerticalStrut(10)); //add gaps between touples
+        }
+    }
+
     private JPanel createCategoryPanel() {
         // The panel that contains the category buttons (BoxLayout for vertical stack)
         JPanel categoryPanel = new JPanel();
@@ -366,7 +597,7 @@ public class GUIBorrowItemPanel extends JPanel {
         // Ensure the back button is visible and has appropriate size
         backButton.setText("Go Back");
         addToBasketButton = new JButton("Add To Basket");
-        JButton continueButton = new JButton("Continue");
+        continueButton = new JButton("Continue");
         
         styleActionButton(backButton);
         styleActionButton(addToBasketButton);
@@ -397,6 +628,11 @@ public class GUIBorrowItemPanel extends JPanel {
                 }
             }
         });
+
+        continueButton.addActionListener(e -> {
+            updateBasketDisplayPanel();
+            cardLayout.next(GUIBorrowItemPanel.this);
+        });
         
         // Ensure all buttons are added properly to the button panel
         buttonPanel.add(backButton);
@@ -424,4 +660,5 @@ public class GUIBorrowItemPanel extends JPanel {
             return panel;
         }
     }
+
 }
